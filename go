6ac078c314237b62241c8f6765f7f73e0af863fd
Starli0n <Starli0n@users.noreply.github.com>
2020-11-04 21:16:21 +0100

    Install ssh and docker-compose

--- go
+++ go
@@ -6,4 +6,15 @@
 # Activate WOL
 ethtool -s eth0 wol g
 
+# docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+# sudo cp /usr/local/bin/docker-compose /boot/config/custom/docker-compose
+cp /boot/config/custom/docker-compose /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 touch /root/go.done
+
+export SUBJECT="go"
+export DESCRIPTION="Boot... 🤖"
+bash /boot/config/plugins/dynamix/notifications/agents/Telegram.sh
--- install.sh
+++ install.sh
@@ -6,14 +6,29 @@ install-go () {
 	cat ../go
 }
 
+install-ssh () { # /etc/rc.d/rc.sshd restart
+    echo "Copy public keys to /boot/config/ssh/root/authorized_keys file"
+    touch /boot/config/ssh/root/authorized_keys # --> /root/.ssh/authorized_keys
+    echo "Backup /etc/ssh/sshd_config -->> /boot/config/ssh/sshd_config"
+    cp /etc/ssh/sshd_config /boot/config/ssh/sshd_config.bak
+}
+
 install-user-scripts () {
     # /boot/config/custom/user.scripts/ -->> /boot/config/plugins/user.scripts/scripts/
     cp -avr user.scripts/* ../plugins/user.scripts/scripts
 }
 
+install-docker-compose () {
+    curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    cp /usr/local/bin/docker-compose /boot/config/custom/docker-compose
+}
+
 install () {
     install-go
+    install-ssh
     install-user-scripts
+    install-docker-compose
 }
 
 install
